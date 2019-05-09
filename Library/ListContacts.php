90ad913df28ec2288a4ae2b0575dc9f592ade4f2
SerhiiKozak <sergiykoza4ok@gmail.com>
2019-05-09 12:37:44 +0300

    fixed path problems

--- Library/ListContacts.php
+++ Library/ListContacts.php
@@ -109,10 +109,7 @@ class ListContacts extends Db {
         foreach ($lists as $key => $data) {
             if ($data['list_name'] == $name) {
                 return true;
-                break;
-            } else {
-                return false;
-            }
+            } 
         }
     }
 }
