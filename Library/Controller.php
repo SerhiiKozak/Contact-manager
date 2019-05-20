d04fd2d30bc96754c474187f25a1b4d63d6c3ed8
SerhiiKozak <sergiykoza4ok@gmail.com>
2019-05-20 18:29:36 +0300

    CSS was added

--- Library/Controller.php
+++ Library/Controller.php
@@ -4,7 +4,6 @@ require_once ROOT_PATH . '/Library/Session.php';
 
 abstract class Controller {
   protected $request = null;
-  protected $response = null;
 
   /**
    * @var Session
@@ -13,13 +12,11 @@ abstract class Controller {
 
   public function __construct() {
     $this->request = $this->getData();
-    $this->response = $this->setData();
     $this->session = Session::getInstance();
   }
   protected function isAuterized() {
     return empty($this->session->get('CONTACT_USER'));
   }
   public function getData() {}
-  public function setData() {}
   public function show() {}
 }
